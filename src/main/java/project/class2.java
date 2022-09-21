bf791024cf1309fbdb30f4805cfdb37253a1da8d
ujwalnarayana123 <111624915+ujwalnarayana123@users.noreply.github.com>
2022-09-21 22:18:09 +0530

    project  1 commit

---
+++ src/main/java/project/class2.java
@@ -0,0 +1,12 @@
+package project;
+
+public class class2 {
+
+	public static void main(String[] args) {
+		System.out.println(" ujwal");
+		System.out.println(" sudha");
+		System.out.println(" manoj256");
+
+	}
+
+}
