2d44cf98d6eaf0506d859e83e3445d81d47ac1c1
ujwalnarayana123 <111624915+ujwalnarayana123@users.noreply.github.com>
2022-09-23 19:44:21 +0530

    added new class

---
+++ src/main/java/project/mainclass.java
@@ -0,0 +1,16 @@
+package project;
+
+public class mainclass {
+
+	public static void main(String[] args) {
+		System.out.println(" ujwal");
+		System.out.println(" sudha");
+		System.out.println(" manoj256");
+			System.out.println(" mrf");
+			System.out.println(" appollo");
+			System.out.println(" ceat");
+
+		}
+
+
+	}
